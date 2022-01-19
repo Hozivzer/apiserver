b49aecac015efc2d1ca57712313316eeb4b7e598
Hozivzer <15102264296@163.com>
2022-01-20 01:41:59 +0800

    auth

--- router/router.go
+++ router/router.go
@@ -25,7 +25,11 @@ func Load(g *gin.Engine, mw ...gin.HandlerFunc) *gin.Engine {
 
 	u := g.Group("/v1/user")
 	{
-		u.POST("/:username", user.Create)
+		u.POST("", user.Create)
+		u.DELETE("/:id", user.Delete)
+		u.PUT("/:id", user.Update)
+		u.GET("", user.List)
+		u.GET("/:usernmae", user.Get)
 	}
 
 	//the health check handlers
